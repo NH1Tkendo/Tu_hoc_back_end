40c020e3a205f40ab4854399a6da293280acdc3f
NH1Tkendo <ngobatai1711@gmail.com>
2025-06-06 19:40:54 +0700

    Update

---
+++ ThucHanh_JavaScript/string.js
@@ -0,0 +1,20 @@
+let string1 = "Day la chuoi 1"
+let string2 = new String("Day la chuoi 2")
+
+//charAt():-------------------------
+//console.log(string1.charAt(1))
+//==================================
+
+//String indexing: -----------------
+//console.log(string2[2])
+//==================================
+
+//So sánh chuỗi --------------------
+// if(string1 > string2){
+//     console.log("1 > 2")
+// } else {
+//     console.log("2 > 1")
+// }
+
+// console.log(string1 === string2)
+//==================================
